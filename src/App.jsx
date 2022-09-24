1aa3e60d146a0f65ef68f95f375ef0460446d553
Wilson Lluilema <wilsonlluilema007@gmail.com>
2022-09-24 02:23:03 -0500

    envio ,creacion y configuracion

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Container } from "react-bootstrap";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1 className="py-5">Buscador de Bebidas</h1>
+      </header>
+      <Container className="mt-5">1</Container>
+    </>
+  );
+}
+
+export default App;
